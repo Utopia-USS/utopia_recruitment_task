e7eb58c92aedb9b43858408b2e83241f4b41d7c9
Viktor Kirjanov <viktor.kirjanov@gmail.com>
2022-10-14 09:48:12 +0200

    tests

---
+++ test/models/email_model_test.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter_test/flutter_test.dart';
+import 'package:utopia_recruitment_task/models/email_model.dart';
+
+void main() {
+  const validEmailString = 'email@gmail.com';
+  const invalidEmailString = 'email';
+  group('Email', () {
+    group('constructors', () {
+      test('pure creates correct instance', () {
+        final email = Email.pure();
+        expect(email.value, '');
+        expect(email.pure, true);
+      });
+
+      test('dirty creates correct instance', () {
+        final email = Email.dirty(validEmailString);
+        expect(email.value, validEmailString);
+        expect(email.pure, false);
+      });
+    });
+
+    group('validator', () {
+      test('returns empty error when email is empty', () {
+        expect(
+          Email.dirty().error,
+          EmailValidationError.invalid,
+        );
+      });
+
+      test('is valid when email is not empty', () {
+        expect(
+          Email.dirty(validEmailString).error,
+          isNull,
+        );
+      });
+
+      test('is invalid when email is not valid', () {
+        expect(
+          Email.dirty(invalidEmailString).error,
+          isNotNull,
+        );
+      });
+    });
+  });
+}
